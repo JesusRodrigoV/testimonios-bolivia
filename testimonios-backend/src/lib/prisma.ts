716ac2f11d3c7fb3a743391cff003340e6ea3475
Manuel Franco Jiménez Mendoza <manuel.jimenez@ucb.edu.bo>
2026-06-02 14:37:34 -0400

    cambios3

--- testimonios-backend/src/lib/prisma.ts
+++ testimonios-backend/src/lib/prisma.ts
@@ -1,5 +1,12 @@
 import { PrismaClient } from "@generated/prisma";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  log: [
+    { emit: 'stdout', level: 'query' },
+    { emit: 'stdout', level: 'info' },
+    { emit: 'stdout', level: 'warn' },
+    { emit: 'stdout', level: 'error' },
+  ],
+});
 
 export default prisma;
